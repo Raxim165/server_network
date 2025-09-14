d9cf4ee5db2e0a4153645cb60db3ff00e1c8d488
Raxim <rahtag165@gmail.com>
2025-09-14 21:59:02 +0300

    change package.json

--- src/index.js
+++ src/index.js
@@ -172,6 +172,6 @@ clientPromise.then(client => {
 
 app.use((err, req, res) => res.status(500).send(err.message));
 
-const port = 3000;
-server.listen(port, () => log(`http://127.1.0.1:${port}`));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => log(`http://127.1.0.1:${PORT}`));
 
